80f9379b72da42311b5bd4bdd0b563aac81016bb
wo9374 <wo9374@naver.com>
2020-10-22 15:02:36 +0900

    okhttp에 글라이드,피카소 사용

--- app/src/main/java/com/example/networksample/NetworkAPI_Library/OKHttpExample.kt
+++ app/src/main/java/com/example/networksample/NetworkAPI_Library/OKHttpExample.kt
@@ -3,7 +3,9 @@ package com.example.networksample.NetworkAPI_Library
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.example.networksample.R
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_networking_result.*
 import okhttp3.*
 import org.json.JSONException
@@ -11,8 +13,8 @@ import org.json.JSONObject
 import java.io.IOException
 
 class OKHttpExample : AppCompatActivity() {
-    private val url =
-        "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&type=restaurant&keyword=cruise&key=AIzaSyBrJ3ec9wTuS6L-xHkaXLU8BJbFsx_LZ9o"
+    private val url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?" +
+            "location=-33.8670522,151.1957362&radius=500&type=restaurant&keyword=cruise&key=AIzaSyBrJ3ec9wTuS6L-xHkaXLU8BJbFsx_LZ9o"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,6 +47,15 @@ class OKHttpExample : AppCompatActivity() {
                             formattedResult.append("${responseJSONArray.getJSONObject(i)["name"]} => ${responseJSONArray.getJSONObject(i)["rating"]} \n")
                         }
                         tv_result.text = "List of Restaurants \nName\tRating \n \n$formattedResult"
+
+                        Glide.with(applicationContext)
+                            .load(responseJSONArray.getJSONObject(0)["icon"])  //불러올 이미지 http주소
+                            .placeholder(R.drawable.ic_launcher_background) //사진 로딩 전 이미지
+                            .error(R.drawable.ic_launcher_background) //사진 불러오지 못했을 때 이미지
+                            .into(img_result) // 지정할 이미지뷰
+
+                        //Picasso.get().load(responseJSONArray.getJSONObject(0)["icon"].toString()).into(img_result)
+
                     } catch (e: JSONException) {
                         e.printStackTrace()
                     }
--- app/src/main/java/com/example/networksample/NetworkAPI_Library/VolleyExample.kt
+++ app/src/main/java/com/example/networksample/NetworkAPI_Library/VolleyExample.kt
@@ -9,28 +9,34 @@ import com.android.volley.toolbox.Volley
 import com.example.networksample.R
 import kotlinx.android.synthetic.main.activity_networking_result.*
 import org.json.JSONException
+import java.net.URL
 
 class VolleyExample : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_networking_result)
 
-        val requestQueue = Volley.newRequestQueue(this) //Queue Start
-        val url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&type=restaurant&keyword=cruise&key=AIzaSyBrJ3ec9wTuS6L-xHkaXLU8BJbFsx_LZ9o"
+        //onCreate가 실행되면 RequestQueue에 요청이 전달되고 Volley가 자동으로 웹서버에 Request를 전달하여 응답을 받아와 화면에 출력.
 
-        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null, { response ->
-            Log.d("요청결과", "VolleyExample onResponse")
-            try {
-                val formattedResult = StringBuilder()
-                val responseJSONArray = response.getJSONArray("results")
-                for (i in 0 until responseJSONArray.length()) {
-                    formattedResult.append("${responseJSONArray.getJSONObject(i)["name"]} => ${responseJSONArray.getJSONObject(i)["rating"]} \n")
+        val requestQueue = Volley.newRequestQueue(this) //리퀘스트 요청객체 생성
+        val url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?" +
+                "location=-33.8670522,151.1957362&radius=500&type=restaurant&keyword=cruise&key=AIzaSyBrJ3ec9wTuS6L-xHkaXLU8BJbFsx_LZ9o"
+
+        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
+            { response -> //(람다) 응답 받았을 때 Response.Listener 자동 호출
+                Log.d("요청결과", "VolleyExample onResponse")
+                try {
+                    val formattedResult = StringBuilder()
+                    val responseJSONArray = response.getJSONArray("results")
+                    for (i in 0 until responseJSONArray.length()) {
+                        formattedResult.append("${responseJSONArray.getJSONObject(i)["name"]} => ${responseJSONArray.getJSONObject(i)["rating"]} \n")
+                    }
+                    tv_result.text = "List of Restaurants \nName\tRating \n \n$formattedResult"
+                } catch (e: JSONException) {
+                    e.printStackTrace()
                 }
-                tv_result.text = "List of Restaurants \nName\tRating \n \n$formattedResult"
-            } catch (e: JSONException) {
-                e.printStackTrace()
-            }
-        }) { error ->
+            })
+        { error -> //(람다) 에러 발생시 Response.ErrorListener 호출
             Log.d("요청결과", "OkHttpExample onErrorResponse$error")
         }
         requestQueue.add(jsonObjectRequest)
